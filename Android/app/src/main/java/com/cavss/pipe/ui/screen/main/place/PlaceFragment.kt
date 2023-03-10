749580d8dc9a9ebb03a96c20135aea323c4d5f90
cavss <cavss1118@gmail.com>
2023-03-10 23:25:33 +0900

    #0018 place fragment의 메뉴 xml

--- Android/app/src/main/java/com/cavss/pipe/ui/screen/main/place/PlaceFragment.kt
+++ Android/app/src/main/java/com/cavss/pipe/ui/screen/main/place/PlaceFragment.kt
@@ -7,25 +7,42 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.viewpager2.widget.ViewPager2
 import com.cavss.pipe.R
 import com.cavss.pipe.databinding.FragmentPlaceBinding
+import com.cavss.pipe.ui.custom.viewpager.BaseFragmentAdapter
 import com.cavss.pipe.ui.screen.main.MainBottomNaviVM
+import com.cavss.pipe.ui.screen.main.my.MyFragment
+import com.cavss.pipe.ui.screen.main.place.frags.MyHomeFragment
+import com.cavss.pipe.ui.screen.main.place.frags.OfficeFragment
+import com.cavss.pipe.ui.screen.main.place.frags.PreSaleFragment
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayout.*
 import com.google.android.material.tabs.TabLayoutMediator
 
 class PlaceFragment : Fragment(){
 
-    private val mainBottomNaviVM : MainBottomNaviVM by activityViewModels()
-
-    private lateinit var binding : FragmentPlaceBinding
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentPlaceBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    private fun setTabLayout(tabLayout: TabLayout){
+    private var myHomeFragment : MyHomeFragment? = null
+    private var preSaleFragment : PreSaleFragment? = null
+    private var officeFragment : OfficeFragment? = null
+    private fun setTabLayoutWithViewPager2(tabLayout: TabLayout, viewPager2: ViewPager2){
         try{
+            myHomeFragment = MyHomeFragment()
+            preSaleFragment = PreSaleFragment()
+            officeFragment = OfficeFragment()
+            viewPager2.let {
+                var viewpagerAdapter =  object : BaseFragmentAdapter.Adapter(requireActivity()){
+                    override fun setFragmentList(): List<Fragment> {
+                        return listOf<Fragment>(
+                            myHomeFragment ?: MyFragment(),
+                            preSaleFragment ?: PreSaleFragment(),
+                            officeFragment ?: OfficeFragment()
+                        )
+                    }
+                }
+                it.adapter = viewpagerAdapter
+            }
+
             tabLayout.let {
                 it.tabMode = MODE_FIXED
                 it.tabGravity = GRAVITY_FILL
@@ -33,10 +50,13 @@ class PlaceFragment : Fragment(){
             TabLayoutMediator(binding.placeTablayout, binding.placeViewpager2){ tab, position ->
                 when(position){
                     0 -> {
-                        tab.text = requireContext().getString(R.string.place_tab_item_subscription_presale)
+                        tab.text = requireContext().getString(R.string.place_tab_item_myhome)
                     }
                     1 -> {
-                        tab.text = requireContext().getString(R.string.place_tab_item_myhome)
+                        tab.text = requireContext().getString(R.string.place_tab_item_subscription_presale)
+                    }
+                    2 -> {
+                        tab.text = requireContext().getString(R.string.place_tab_item_office)
                     }
                 }
             }.attach()
@@ -44,6 +64,23 @@ class PlaceFragment : Fragment(){
             Log.e("mException", "PlaceFragment, setTabLayout // Exception : ${e.localizedMessage}")
         }
     }
+
+
+    private val mainBottomNaviVM : MainBottomNaviVM by activityViewModels()
+
+    private lateinit var binding : FragmentPlaceBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentPlaceBinding.inflate(inflater, container, false)
+        binding.run {
+            setTabLayoutWithViewPager2(
+                tabLayout = this@run.placeTablayout,
+                viewPager2 = this@run.placeViewpager2
+            )
+        }
+        return binding.root
+    }
+
+
     override fun onStart() {
         super.onStart()
         /*
@@ -51,7 +88,7 @@ class PlaceFragment : Fragment(){
         사용자에게 프래그먼트가 보이게 되고, 이 메서드에서 UI를 관리하는 코드를 초기화 합니다.
         이 메서드는 매우 빠르게 완료되고, 완료되면 Resumed(재개)상태로 들어가 onResume() 메서드를 호출합니다.
          */
-        setTabLayout(tabLayout = binding.placeTablayout)
+
     }
 
     override fun onResume() {
@@ -81,5 +118,8 @@ class PlaceFragment : Fragment(){
         액티비티가 중단되면 액티비티 객체는 메모리 안에 머무르게 됩니다. 이 객체가 모든 상태 및 멤버 정보를 가지고 관리하지만 연결되 있지는 않습니다. 다시 시작되면 이 객체를 다시 호출합니다. 최상위 상태가 재개되어도 콜백 메서드 중에 생성된 요소는 다시 초기화할 필요가 없습니다. 또한 시스템은 레이아웃에 있는 각 View 객체의 현재 상태도 기록하므로 사용자가 EditText 위젯에 텍스트를 입력했다면 내용이 저장되기 때문에 저장, 복원할 필요가 없습니다.
         프래그먼트는 이를 제거하는 트랜잭션에서 addToBackStack()을 호출하여 인스턴스를 저장하라고 명시적으로 요청할 경우에만 액티비티에서 관리하는 백 스택으로 들어갑니다.
          */
+        myHomeFragment = null
+        preSaleFragment = null
+        officeFragment = null
     }
 }
